2777194f0f7f89278cb0c5d68510a7601884410b
Josh Lee <jleedev@gmail.com>
2021-12-09 19:31:46 +0000

    Add a basic html page for the app container

--- build.js
+++ build.js
@@ -1,23 +1,27 @@
-import esbuild from 'esbuild';
+import esbuild from "esbuild";
 
 console.log(performance.now());
 
-const buildJs = esbuild.build({
-  entryPoints: ['src/main.js'],
-  outdir: 'dist/',
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-});
-
-const buildCss = esbuild.build({
-  entryPoints: ['node_modules/maplibre-gl/dist/maplibre-gl.css'],
-  outdir: 'dist/',
-});
-
-console.log(...await Promise.all([
-  buildJs,
-  buildCss,
-]));
+console.log(
+  ...(await Promise.all([
+    esbuild.build({
+      entryPoints: [
+        "src/main.js",
+        "node_modules/bootstrap/dist/js/bootstrap.bundle.js",
+      ],
+      outdir: "dist/",
+      //bundle: true,
+      minify: true,
+      sourcemap: true,
+    }),
+    esbuild.build({
+      entryPoints: [
+        "node_modules/maplibre-gl/dist/maplibre-gl.css",
+        "node_modules/bootstrap/dist/css/bootstrap.min.css",
+      ],
+      outdir: "dist/",
+    }),
+  ]))
+);
 
 console.log(performance.now());
