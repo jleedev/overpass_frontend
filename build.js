d55b0d9a43debf487740f7b929d98372c691c9ab
Josh Lee <jleedev@gmail.com>
2022-06-30 16:33:45 +0000

    fix build

--- build.js
+++ build.js
@@ -2,42 +2,36 @@ import {} from "dotenv/config";
 import fs from "fs/promises";
 import esbuild from "esbuild";
 
-performance.mark('build');
+await fs.mkdir("dist/", { recursive: true });
 
-await fs.mkdir('dist/', { recursive: true });
-
-try {
-  await Promise.all([
-    esbuild.build({
-      entryPoints: ["src/main.js"],
-      define: {
-        MAPTILER_KEY: `"${process.env.MAPTILER_KEY}"`,
-      },
-      outdir: "dist/",
-      bundle: true,
-      minify: true,
-      sourcemap: true,
-      format: "esm",
-    }),
-    esbuild.build({
-      entryPoints: ["src/overpass_worker.js"],
-      outdir: "dist/",
-      bundle: true,
-      minify: true,
-      sourcemap: true,
-    }),
-    esbuild.build({
-      entryPoints: [
-        "node_modules/maplibre-gl/dist/maplibre-gl.css",
-        "node_modules/bootstrap/dist/css/bootstrap.min.css",
-      ],
-      outdir: "dist/",
-    }),
-    fs.copyFile("index.html", "dist/index.html"),
-  ]);
-} catch (err) {
-  console.error(err);
-  process.exit(1);
-}
-
-console.log(performance.measure('build').duration);
+await Promise.all([
+  esbuild.build({
+    entryPoints: ["src/main.js"],
+    define: {
+      MAPTILER_KEY: `"${process.env.MAPTILER_KEY}"`,
+    },
+    outdir: "dist/",
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    format: "esm",
+    logLevel: "info",
+  }),
+  esbuild.build({
+    entryPoints: ["src/overpass_worker.js"],
+    outdir: "dist/",
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    logLevel: "info",
+  }),
+  esbuild.build({
+    entryPoints: [
+      "node_modules/maplibre-gl/dist/maplibre-gl.css",
+      "node_modules/bootstrap/dist/css/bootstrap.min.css",
+    ],
+    outdir: "dist/",
+    logLevel: "info",
+  }),
+  fs.copyFile("index.html", "dist/index.html"),
+]);
